ef0809266a76630635bd2a6d69b6a2f0c84f43d7
Dale Karp <me@dale.io>
2013-04-07 19:04:21 -0400

    removed apiOnlyMode as it requires a lot of effort to implement

--- config/project.js
+++ config/project.js
@@ -14,10 +14,6 @@ module.exports = function(app)
   app.set('databaseName', 'test');
   // apiPrefix: URL prefix for API routes
   app.set('apiPrefix', '/api');
-  // apiOnlyMode: if true, the application will only
-  // listen and respond to to API requests. session support 
-  // & serving pages will be disabled.
-  app.set('apiOnlyMode', false);
 
   ///////////////////////////////////////////////////////////////////
   ///////////// DO NOT ALTER BELOW THIS POINT ///////////////////////
@@ -29,31 +25,26 @@ module.exports = function(app)
   app.use(express.bodyParser());
   // enable express cookie parsing
   app.use(express.cookieParser());
-  // set up session store (client/api mode only)
-  if(app.get('apiOnlyMode') === true) {
-    // use MongoStore if in production mode
-    if (app.get('env') === 'production') {
-      app.use(express.session({ secret: 'lolcats',
-                              store: new MongoStore({
-                                db: app.get('databaseName')
-                              })
-      }));
-    }
-    // use MemoryStore if in development mode
-    else {
-      app.use(express.session({ secret: 'lolcats',
-                              store: express.session.MemoryStore({
-                                reapInterval: 60000 * 10
-                              })
-      }));
-    }
+  // use MongoStore if in production mode
+  if (app.get('env') === 'production') {
+    app.use(express.session({ secret: 'lolcats',
+                            store: new MongoStore({
+                              db: app.get('databaseName')
+                            })
+    }));
+  }
+  // use MemoryStore if in development mode
+  else {
+    app.use(express.session({ secret: 'lolcats',
+                            store: express.session.MemoryStore({
+                              reapInterval: 60000 * 10
+                            })
+    }));
   }
   // initialize passport
   app.use(passport.initialize());
   // initialize passport session
   // && serve client (client/api mode only)
-  if(app.get('apiOnlyMode') === false) {
-    app.use(passport.session());
-    app.use(express.static(__dirname + '/client/app'));
-  }
+  app.use(passport.session());
+  app.use(express.static(__dirname + '/client/app'));
 }
