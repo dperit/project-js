acba9adb10acaa966f031081af791c839e34d033
Dale Karp <me@dale.io>
2013-04-09 12:34:31 -0400

    hack to make sure roles are added

--- app.js
+++ app.js
@@ -22,3 +22,24 @@ var port = app.get('port');
 app.listen(port);
 console.log("Project.js API going online...")
 console.log("Project.js API online: listening on port " + port);
+
+// quick hack to make sure
+// roles exist
+var Role = mongoose.model('Role');
+Role.find({}, function(err, role) {
+  if(err) res.send(500, err);
+  if(role.length === 0) {
+    console.log('empty');
+    var dev = new Role({title: 'Developer'});
+    dev.save();
+    var projectManager = new Role({title: 'Project Manager'});
+    projectManager.save();
+    var productManager = new Role({title: 'Product Manager'});
+    productManager.save();
+    var projectLeader = new Role({title: 'Project Leader'});
+    projectLeader.save();
+    var qaSpecialist = new Role({title: 'QA Specialist'});
+    qaSpecialist.save();
+  }
+});
+
